7eb9fcce79fbc8cb9a7db8a59fb6a69a990f4a78
20020314 <yitianyangtwo@qq.com>
2018-09-21 02:10:00 +0800

    update pic.sh
    增加key

--- pic.sh
+++ pic.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-serverchan_sckey=`nvram get serverchan_sckey`
+serverchan_sckey=`SCU27196Tc53e77380bfced31538be87d17b39efb5b0d246bc4b37`
 
 
 url=$(curl -s "http://gank.io/api/random/data/%E7%A6%8F%E5%88%A9/1" | grep url | awk '{print $2}')
@@ -20,4 +20,4 @@ content="随机妹子图
 
 
 curl -s "http://sc.ftqq.com/$serverchan_sckey.send?text=$title" -d "&desp=$content" 
-logger -t "【福利推送】" "路由器酱推送了一张妹子图"
+logger -t "【福利推送】" "凛酱推送了一张妹子图"
